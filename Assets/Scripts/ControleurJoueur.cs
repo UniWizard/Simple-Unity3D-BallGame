dde1bcb4a16015ff8d8fdc294ff5160fdddb7fae
BearMiscca <bmiscca0613@gmail.com>
2023-09-17 01:35:00 +0900

    BM - simple game

---
+++ Assets/Scripts/ControleurJoueur.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControleurJoueur : MonoBehaviour
+{
+	public float vitesse;
+    public Text countText;
+    public Text winText;
+    private Rigidbody rb;
+    private int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+      rb = GetComponent<Rigidbody>();  
+      count = 0;
+      SetCountText ();
+      winText.text = "";
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 mouvement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(mouvement*vitesse);
+
+    }
+    public void OnTriggerEnter (Collider other) {
+        if (other.gameObject.CompareTag ("Cible")) {
+              other.gameObject.SetActive (false) ;
+              count = count + 1;
+              SetCountText ();
+        }
+    }
+    
+    void SetCountText (){
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 8)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
+
+}
